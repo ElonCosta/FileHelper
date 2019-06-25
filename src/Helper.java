b7114c2e08ac4e23fb0e028fb461ab9196d1ec4e
Elon_Costa <santos.elon@gmail.com>
2019-06-24 22:42:27 -0300

    Second Commit

--- src/Helper.java
+++ src/Helper.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.text.SimpleDateFormat;
@@ -8,10 +10,7 @@ import org.apache.commons.io.*;
 public class Helper {
 
     File source = new File("Source");
-    File archive = new File("Source/Archive");
-    File yearArchive = new File(archive.toString()+"/"+(new SimpleDateFormat("yyyy").format(new Date())));
-    File monthArchive = new File(yearArchive.toString() + "/" + (new SimpleDateFormat("MM").format(new Date())));
-    File dayArchive = new File(monthArchive.toString() + "/" + (new SimpleDateFormat("dd").format(new Date())));
+    File archive = new File("Source/Archive/" + (new SimpleDateFormat("yyyy/MM/dd").format(new Date())));
     File latest = new File("Source/Latest");
 
 
@@ -22,15 +21,9 @@ public class Helper {
     public void checker(File file){
         File inLatest = new File(latest.toString() + "/" + file.getName());
         if (inLatest.getName().equals(file.getName())){
-            if(!checker(inLatest,file));{
-                try {
-                    FileUtils.deleteDirectory(inLatest);
-                }catch (Exception e){
-
-                }
+            if(!checker(inLatest,file)){
                 createFile(file);
             }
-
         }
     }
 
@@ -45,6 +38,10 @@ public class Helper {
             }
         }else{
             try{
+                File arch = new File(archive.toString() + (new SimpleDateFormat("/hh_mm")).format(new Date()) + "/" + inLatest.getName());
+                System.out.println("Archiving \"" + arch.getName() + "\" on" + archive.toString() + (new SimpleDateFormat("\"hh_mm")).format(new Date()));
+                arch.mkdirs();
+                FileUtils.copyDirectory(inLatest,arch);
                 FileUtils.deleteDirectory(inLatest);
             }catch (Exception e){
                 System.err.println(e);
@@ -54,41 +51,56 @@ public class Helper {
     }
 
     private boolean checker(File file1, File file2){
-            if (file1.isDirectory() && file2.isDirectory()){
-                File[] file1Arr = file1.listFiles();
-                File[] file2Arr = file2.listFiles();
-                if (file1Arr.length == file2Arr.length){
-                    for (int i = 0; i < file1Arr.length; i++){
-                        System.out.println(file1Arr[i].getName() + "|" + file2Arr[i].getName());
-                        if (!checker(file1Arr[i],file2Arr[i])){
-                            return false;
-                        }
+        if (file1.isDirectory() && file2.isDirectory()){
+            File[] file1Arr = file1.listFiles();
+            File[] file2Arr = file2.listFiles();
+            if (file1Arr.length == file2Arr.length){
+                for (int i = 0; i < file1Arr.length; i++){
+                    if (!file1Arr[i].getName().equals(file2Arr[i].getName())){
+                        return false;
                     }
-                }else {
-                    return false;
+                    if (!checker(file1Arr[i],file2Arr[i])){
+                        return false;
+                    }
                 }
+            }else {
+                return false;
             }
-            return true;
+        }else if (file1.isFile() && file2.isFile()){
+            if (!file1.getName().equals(file2.getName())){
+                return false;
+            }
+            try{
+                BufferedReader br1 = new BufferedReader(new FileReader(file1));
+                BufferedReader br2 = new BufferedReader(new FileReader(file2));
+
+                String str1 = "";
+                while (br1.ready()){
+                    str1 += br1.readLine() + "\n";
+                }
+                br1.close();
+                String str2 = "";
+                while (br2.ready()){
+                    str2 += br2.readLine() + "\n";
+                }
+                br2.close();
+                return str1.equals(str2);
+            }catch (Exception e){
+
+            }
+        }
+        return true;
     }
 
     private void createFolders(){
         if (this.source.mkdir()){
-            System.out.println("Creating source folder");
+            System.out.println("Creating " + source.toString());
         }
-        if (archive.mkdir()){
-            System.out.println("Creating archive folder");
+        if (archive.mkdirs()){
+            System.out.println("Creating " + archive.toString());
         }
         if (latest.mkdir()){
-            System.out.println("Creating \"latest\" folder");
-        }
-        if (yearArchive.mkdir()){
-            System.out.println("Creating " + yearArchive.getName() + " Archive Folder");
-        }
-        if (monthArchive.mkdir()){
-            System.out.println("Creating " + monthArchive.getName() + " Archive Folder");
-        }
-        if (dayArchive.mkdir()){
-            System.out.println("Creating " + dayArchive.getName() + " Archive Folder");
+            System.out.println("Creating " + latest.toString());
         }
     }
 }
--- src/Launcher.java
+++ src/Launcher.java
@@ -1,8 +1,16 @@
 import java.io.File;
+import java.util.Timer;
 
 public class Launcher {
     public static void main(String[] args) {
         Helper helper = new Helper();
+        Long time = System.currentTimeMillis();
         helper.checker(new File("AAA"));
+        while (true){
+            if (System.currentTimeMillis() >= time+300000){
+                helper.checker(new File("AAA"));
+                time = System.currentTimeMillis();
+            }
+        }
     }
 }
